e401da1580a56dd460729e450d9fe205e9ba6eac
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-08 00:16:25 -0600

    gets random song id

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ var db = admin.firestore();
 
 db.collection('canciones').get()
   .then((snapshot) => {
+    const size = snapshot.size;
+    const song_ids = [];
     snapshot.forEach((doc) => {
       const data = doc.data();
-      console.log(doc.id, '=>', data);
+      song_ids.push(doc.id);
     });
+    const randomIndex = Math.floor(Math.random() * size);
+    console.log(song_ids[randomIndex]);
   })
   .catch((err) => {
     console.log('Error getting documents', err);
