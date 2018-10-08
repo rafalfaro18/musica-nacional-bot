656a90e5bf00ae4992f1dda6ebd311978fa50eb5
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-08 00:33:03 -0600

    gets a random song

--- index.js
+++ index.js
@@ -15,12 +15,21 @@ db.collection('canciones').get()
   .then((snapshot) => {
     const size = snapshot.size;
     const song_ids = [];
+    const songs = [];
     snapshot.forEach((doc) => {
       const data = doc.data();
+      const id = doc.id;
+      songs[id] = {
+        nombre: data.nombre
+      };
       song_ids.push(doc.id);
     });
     const randomIndex = Math.floor(Math.random() * size);
-    console.log(song_ids[randomIndex]);
+    const randomId = song_ids[randomIndex];
+    return songs[randomId];
+  })
+  .then((song) => {
+    console.log(song);
   })
   .catch((err) => {
     console.log('Error getting documents', err);
