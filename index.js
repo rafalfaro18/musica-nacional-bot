9b0e2859f985ebfdbd38d06e55d664829f60835f
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-08 20:58:23 -0600

    Adds twitter

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const admin = require('firebase-admin');
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 var serviceAccount = require('./key.json');
 
